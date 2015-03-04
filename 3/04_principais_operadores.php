a0f60f4daa178414d81702b4712cb0677629d5b3
robertooliveiras <robertooliveira2604@gmail.com>
2015-03-04 00:46:52 -0300

    até a aula 6

---
+++ 3/04_principais_operadores.php
@@ -0,0 +1,71 @@
+﻿<?php
+
+$x = 10;
+$y = 5;
+
+$soma = $x + $y;
+$subtrai = $x - $y;
+$multiplica = $x * $y;
+$divide = $x / $y;
+
+?>
+<h1>Operações matemáticas com PHP</h1>
+<h2>Onde <?php echo "x = " . $x . ", e y = " . $y; ?> </h2>
+<table>
+	<tr>
+		<td>soma: </td>
+		<td><?=$soma ?></td>
+	</tr>
+	<tr>
+		<td>Subtração: </td>
+		<td><?=$subtrai ?></td>
+	</tr>
+	<tr>
+		<td>Multiplicação: </td>
+		<td><?=$multiplica ?></td>
+	</tr>
+	<tr>
+		<td>Divisão: </td>
+		<td><?=$divide ?></td>
+	</tr>	
+	<tr>
+		<td>incremental de x (x++), quando x = <?=$x ?>: </td>
+		<td><?=$x++ ?></td>
+	</tr>
+	<tr>
+		<td>incremental de x (x+=5), quando x = <?=$x ?>: </td>
+		<td><?=$x+=5 ?></td>
+	</tr>
+	<tr>
+		<td>decremental de x (x--), quando x = <?=$x ?>: </td>
+		<td><?=$x-- ?></td>
+	</tr>
+	<tr>
+		<td>decremental de x (x-=2), quando x = <?=$x ?>: </td>
+		<td><?=$x-=2 ?></td>
+	</tr>
+	<tr>
+		<td>concatenação: </td>
+		<td><?=$x . " é o resultado atual de x." ?></td>
+	</tr>
+	<tr>
+		<td>concatenação2: </td>
+		<td>
+			<?php
+			$a = "School";
+			$b = " of Net";
+			echo $a.$b;
+			?>
+		</td>
+	</tr>
+	<tr>
+		<td>concatenação3: </td>
+		<td>
+			<?php
+			$a = "School";
+			$a .= " of Net";
+			echo $a;
+			?>
+		</td>
+	</tr>
+</table>
